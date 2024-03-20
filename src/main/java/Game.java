5060c3d60f21a4a2af9e2fb7b096ec06130fc9d1
Александр Зайбель <aleksandrzajbel25182@gmail.com>
2024-03-20 10:10:45 +0700

    Created the game class without implementation

---
+++ src/main/java/Game.java
@@ -0,0 +1,13 @@
+import stdin.CharSource;
+
+public class Game {
+
+
+//    public Game(CharSource source, Hangman hangman, String word) {
+//        this.source = source;
+//        this.hangman = hangman;
+//        this.word = word;
+//        this.guess = new StringBuilder();
+//    }
+
+}
